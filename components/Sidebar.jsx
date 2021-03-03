f06b934136a9c1b4c2a48d47b33352b723f96b71
achrizal15 <achrizal15@gmail.com>
2021-03-03 14:36:11 +0700

    Bar Complete

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -5,15 +5,16 @@ export default function Sidebar(props) {
   return (
     <div
       hidden={props.sidebr}
-      className="left-0 fixed shadow-lg w-52 bg-white h-full overflow-y-auto overflow-x-hidden"
+      className="left-0 bottom-0 fixed shadow-lg w-72 bg-white h-full  "
     >
-      <div className="pt-16 ml-8 mr-4">
+      <div className="mt-16 ml-8 p-2 h-full mr-4 overflow-y-auto overflow-x-hidden">
         <h3 className=" font-bold text-blue-700 mb-2">MENU</h3>
         <MenuSidebar
           menu="Dashboard"
           icon="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
           submenu={["Card Pemasukan", "Card Pengeluaran", "Total"]}
         />
+
         <MenuSidebar
           menu="Nasabah"
           icon="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z"
@@ -34,17 +35,16 @@ export default function Sidebar(props) {
             "Total Pengeluaran",
           ]}
         />
-      </div>
 
-      <footer className=" mt-12  pb-2 pl-2  text-center   text-sm italic">
-        Browser Suport
-        <div className='flex space-x-2 items-center justify-center'>
-        <img className="w-4" src="chrome.png" alt="crome" />
-        <img className="w-4" src="internet-explorer.png" alt="explorer" />
-        <img className="w-4" src="mozilla.png" alt="mozilla" />
-        </div>
-       
-      </footer>
+        <footer className=" mt-12  pb-2 pl-2  text-center   text-sm italic">
+          Browser Suport
+          <div className="flex space-x-2 items-center justify-center">
+            <img className="w-4" src="chrome.png" alt="crome" />
+            <img className="w-4" src="internet-explorer.png" alt="explorer" />
+            <img className="w-4" src="mozilla.png" alt="mozilla" />
+          </div>
+        </footer>
+      </div>
     </div>
   );
 }
