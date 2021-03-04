e9961d7f37291e08cf026ada630d29fc5212a65d
achrizal15 <achrizal15@gmail.com>
2021-03-04 11:20:36 +0700

    Update Dashboard

--- pages/Dashboard/Card.jsx
+++ pages/Dashboard/Card.jsx
@@ -6,7 +6,7 @@ export default function Card() {
       <div className="px-5 py-2 text-gray-500 text-lg font-bold">
         Dashboard Card
       </div>
-      <div className="flex py-4 justify-between px-16">
+      <div className="flex py-4 justify-between px-12 md:px-16">
         <div className=" ">
           <h3 className="text-gray-500 py-2">Pengeluaran Cash</h3>
           <div className="flex space-x-4 items-center">
