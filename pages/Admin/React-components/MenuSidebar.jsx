632c45148b56e9b458e5e5d008aed5bb1581cb7f
achrizal15 <achrizal15@gmail.com>
2021-03-05 01:12:04 +0700

    Memory browser

--- pages/Admin/React-components/MenuSidebar.jsx
+++ pages/Admin/React-components/MenuSidebar.jsx
@@ -1,12 +1,15 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Link from "next/link";
 
 export default function MenuSidebar(props) {
+  useEffect(() => {
+    console.log(props.submenu);
+  }, []);
   return (
-    <div className="mt-4">
+    <div className="mt-4 font-mono">
       <div className="p-0.5 mx-1 px-1 shadow-md rounded-md transition border-l-4 hover:border-yellow-200 hover:shadow bg-blue-100 flex justify-between  cursor-pointer duration-300 duration-500 ease-in-out  transform hover:scale-110 ">
         <Link href={props.href}>
-          <div className="focus:outline-none flex w-full justify-between items-center">
+          <div className="focus:outline-none text-gray-600 font-bold  flex w-full justify-between items-center">
             <div className="w-4 text-gray-300 mr-3">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -44,7 +47,21 @@ export default function MenuSidebar(props) {
       <div className="ml-1">
         {/* Sub child */}
         <div className="ml-4 font-light text-sm  cursor-default">
-          
+          {props.submenu.map((value, i) => {
+            return (
+              <div
+                key={i}
+                className={`border-l-2 pl-4  ${
+                  i % 2 === 0 ? ` border-red-200` : ` border-green-200 `
+                } ${
+                  props.submenu.length - 1 === i ? `rounded-bl-md pt-1` : `py-1`
+                }`}
+              >
+                <Link href={value.ref}><a className='hover:text-gray-600 text-gray-300'> {value.subtex} </a></Link>
+                
+              </div>
+            );
+          })}
         </div>
       </div>
     </div>
--- pages/Admin/React-components/Sidebar.jsx
+++ pages/Admin/React-components/Sidebar.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import MenuSidebar from "./MenuSidebar";
 
-
 export default function Sidebar(props) {
   return (
     <div
@@ -12,33 +11,45 @@ export default function Sidebar(props) {
         <h3 className=" font-bold text-blue-700 mb-2">MENU</h3>
 
         <MenuSidebar
-        href='/Admin/Dashboard'
+          href="/Admin/Dashboard"
           menu="Dashboard"
           icon="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
-          submenu={["Card Pemasukan", "Card Pengeluaran", "Total"]}
+          submenu={[
+            { subtex: "Total Nasabah", ref: "/" },
+            { subtex: "Total Uang Masuk", ref: "/" },
+            { subtex: "Total Uang Keluar", ref: "/" },
+          ]}
         />
 
         <MenuSidebar
-        href='/'
+          href="/"
           menu="Nasabah"
           icon="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z"
-          submenu={["Pesan Masuk", "Konfirmasi", "Detail Nasabah"]}
+          submenu={[
+            { subtex: "Pesan Masuk", ref: "/" },
+            { subtex: "Detail Nasabah", ref: "/" },
+            { subtex: "Konfirmasi", ref: "/" },
+          ]}
         />
 
         <MenuSidebar
-        href='/'
+          href="/"
           menu="Pemasukan"
           icon="M13 7h8m0 0v8m0-8l-8 8-4-4-6 6"
-          submenu={["Input Pemasukan", "Detail Pemasukan", "Total Pemasukan"]}
+          submenu={[
+            { subtex: "Input Pemasukan", ref: "/" },
+            { subtex: "Detail Pemasukan", ref: "/" },
+            ,
+          ]}
         />
         <MenuSidebar
-        href='/Auth/login'
+          href="/Auth/login"
           menu="Pengeluaran"
           icon="M13 17h8m0 0V9m0 8l-8-8-4 4-6-6"
           submenu={[
-            "Input Pengeluaran",
-            "Detail Pengeluaran",
-            "Total Pengeluaran",
+            { subtex: "Input Pengeluaran", ref: "/" },
+            { subtex: "Detail Pengeluaran", ref: "/" },
+          
           ]}
         />
 
