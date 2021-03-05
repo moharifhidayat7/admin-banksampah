8826d2fb4c329f89c52270d7a54d6529d3b65c25
achrizal15 <achrizal15@gmail.com>
2021-03-05 09:24:32 +0700

    Admin Dashboard

---
+++ pages/Admin/Nasabah.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Layout from './React-components/Layout'
+
+export default function Nasabah() {
+    return (
+        <Layout title='Nasabah'>
+            Hellow WOrld
+        </Layout>
+    )
+}
