b68ddffd72461cae6ae24b90f04c49a746a646cd
Arif Hidayat <moharifhidayat7@gmail.com>
2021-03-15 23:45:40 +0700

    m

--- components/Menu.jsx
+++ components/Menu.jsx
@@ -1,22 +1,30 @@
-import * as Icons from "heroicons-react";
+import * as Icons from 'heroicons-react';
 
-import List from "./List";
+import List from './List';
 
 export default function Menu() {
-    return (
-        <List>
-            <List.Item
-                route='/Admin'
-                icon={<Icons.HomeOutline size='1rem' />}
-            ></List.Item>
-            <List.Item>
-                <List.SubItem></List.SubItem>
-            </List.Item>
-            <List.Item>
-                <List.SubItem></List.SubItem>
-                <List.SubItem></List.SubItem>
-            </List.Item>
-            <List.Item></List.Item>
-        </List>
-    );
+  return (
+    <List>
+      <List.Item
+        route='/Admin'
+        icon={<Icons.HomeOutline size='1rem' />}
+        title='Dashboard'
+      />
+      <List.Item
+        route='/Admin/Nasabah'
+        icon={<Icons.UserGroup size='1rem' />}
+        title='Nasabah'
+      />
+      <List.Item
+        route='/Admin/Pembelian'
+        icon={<Icons.Cash size='1rem' />}
+        title='Pembelian'
+      />
+      <List.Item
+        route='/Admin/Sampah'
+        icon={<Icons.Trash size='1rem' />}
+        title='Sampah'
+      />
+    </List>
+  );
 }
