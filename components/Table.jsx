fc1b4fa3fc8ba2a552f04e83f43a3b8cb54670d0
Arif Hidayat <moharifhidayat7@gmail.com>
2021-03-15 10:33:54 +0700

    dashboard

--- components/Table.jsx
+++ components/Table.jsx
@@ -1,70 +1,63 @@
-const Row = () => {
-    return (
-        <tr class='h-12 border-gray-300 border-b'>
-            <td class='text-sm pl-8 pr-6 whitespace-no-wrap text-gray-800 dark:text-gray-100 tracking-normal leading-4'></td>
-            <td class='text-sm pl-8 pr-6 whitespace-no-wrap text-gray-800 dark:text-gray-100 tracking-normal leading-4'>
-                #MC10023
-            </td>
-            <td class='text-sm pr-6 whitespace-no-wrap text-gray-800 dark:text-gray-100 tracking-normal leading-4'>
-                Toyota Motors
-            </td>
-            <td class='pr-6 whitespace-no-wrap'>
-                <div class='flex items-center'>
-                    <p class='ml-2 text-gray-800 dark:text-gray-100 tracking-normal leading-4 text-sm'>
-                        Carrie AnthonyCarrie
-                    </p>
-                </div>
-            </td>
-            <td class='text-sm pr-6 whitespace-no-wrap text-gray-800 dark:text-gray-100 tracking-normal leading-4'>
-                $2,500
-            </td>
-            <td class='text-sm pr-6 whitespace-no-wrap text-gray-800 dark:text-gray-100 tracking-normal leading-4'>
-                02.03.20
-            </td>
-        </tr>
-    );
+const TableCol = ({ children }) => {
+  return (
+    <th class='dark:text-gray-400 font-normal pr-6 text-left text-sm tracking-normal leading-4'>
+      {children}
+    </th>
+  );
 };
 
-const Col = ({ children }) => {
-    return (
-        <th class='dark:text-gray-400 font-normal pl-8 pr-6 text-left text-sm tracking-normal leading-4'>
-            {children}
-        </th>
-    );
+const TableHead = ({ children }) => {
+  return (
+    <thead>
+      <tr class='w-full h-12 bg-gray-700 text-white border-gray-300 border-b py-8'>
+        <th class='dark:text-gray-400 font-normal pl-8 text-left text-sm tracking-normal leading-4'></th>
+        {children}
+      </tr>
+    </thead>
+  );
 };
 
-const Head = ({ children }) => {
-    return (
-        <tr class='w-full h-12 bg-gray-700 text-white border-gray-300 border-b py-8'>
-            {children}
-        </tr>
-    );
+const TableCell = ({ children }) => {
+  return (
+    <td class='text-sm pr-6 whitespace-no-wrap text-gray-800 dark:text-gray-100 tracking-normal leading-4'>
+      {children}
+    </td>
+  );
 };
 
-const Table = () => {
-    return (
-        <div class='mx-auto bg-white dark:bg-gray-800 dark:bg-gray-800 shadow rounded'>
-            <div class='w-full overflow-x-scroll xl:overflow-x-hidden'>
-                <table class='min-w-full bg-white dark:bg-gray-800'>
-                    <thead></thead>
-                    <tbody>
-                        <TableRow></TableRow>
-                        <TableRow></TableRow>
-                        <TableRow></TableRow>
-                        <TableRow></TableRow>
-                        <TableRow></TableRow>
-                        <TableRow></TableRow>
-                        <tr class='h-12 border-gray-300 border-b'>
-                            <td
-                                colSpan='5'
-                                class='text-sm text-center pl-8 pr-6 whitespace-no-wrap text-gray-800 dark:text-gray-100 tracking-normal leading-4'
-                            >
-                                <a href='#'>Lihat Semua Transaksi Sampah</a>
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
-        </div>
-    );
+const TableRow = ({ children }) => {
+  return (
+    <tr class='h-12 border-gray-300 border-b'>
+      <td class='text-sm pl-8 whitespace-no-wrap text-gray-800 dark:text-gray-100 tracking-normal leading-4'></td>
+      {children}
+    </tr>
+  );
 };
+const TableBody = ({ children }) => {
+  return (
+    <tbody>
+      {children}
+
+      <tr class='h-12 border-gray-300 border-b'>
+        <td
+          colSpan='6'
+          class='text-sm text-center pl-8 pr-6 whitespace-no-wrap text-gray-800 dark:text-gray-100 tracking-normal leading-4'
+        >
+          <a href='#'>Lihat Semua Transaksi Sampah</a>
+        </td>
+      </tr>
+    </tbody>
+  );
+};
+
+const Table = ({ children }) => {
+  return (
+    <div class='mx-auto bg-white dark:bg-gray-800 dark:bg-gray-800 shadow rounded'>
+      <div class='w-full overflow-x-scroll xl:overflow-x-hidden'>
+        <table class='min-w-full bg-white dark:bg-gray-800'>{children}</table>
+      </div>
+    </div>
+  );
+};
+
+export { Table, TableHead, TableBody, TableCol, TableRow, TableCell };
