6766dc59ab90648002a40b519613da17837ae5f0
Arif Hidayat <moharifhidayat7@gmail.com>
2021-03-24 09:31:40 +0700

    svg

--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -29,11 +29,11 @@ export default function Navbar({ toggleSidebar }) {
                     width='30'
                     height='30'
                     viewBox='0 0 24 24'
-                    stroke-width='1.5'
+                    strokeWidth='1.5'
                     stroke='currentColor'
                     fill='none'
-                    stroke-linecap='round'
-                    stroke-linejoin='round'
+                    strokeLinecap='round'
+                    strokeLinejoin='round'
                 >
                     <path stroke='none' d='M0 0h24v24H0z' />
                     <line x1='4' y1='8' x2='20' y2='8' />
