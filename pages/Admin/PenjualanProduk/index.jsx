6b24783e500a92afb17e4a8714b12bd0334145cc
achrizal15 <achrizal15@gmail.com>
2021-05-28 08:50:16 +0700

    n

---
+++ pages/Admin/PenjualanProduk/index.jsx
@@ -0,0 +1,36 @@
+import PenjualanLayout from "../../../components/Layouts/PenjualanLayout";
+import DashboardCard from "../../../components/DashboardCard";
+import {
+  UserGroupOutline,
+  UserAddOutline,
+  SwitchVerticalOutline,
+  CollectionOutline,
+  CashOutline,
+} from "heroicons-react";
+import { Collection } from "mongoose";
+export default function Index() {
+   return (
+      <PenjualanLayout>
+         <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 gap-4">
+            <DashboardCard
+               borderColor="border-green-400"
+               textColor="text-green-500"
+               icon={
+                  <SwitchVerticalOutline className="text-green-500" size="100%" />
+               }
+               title="Total Pemasukan Penjualan Produk"
+               value="Rp. 353365959"
+            />
+            <DashboardCard
+               borderColor="border-yellow-400"
+               textColor="text-yellow-500"
+               icon={
+                  <CollectionOutline className="text-yellow-500" size="100%" />
+               }
+               title="Total Produk di Etalase"
+               value="250 Produk"
+            />
+         </div>
+      </PenjualanLayout>
+   );
+}
