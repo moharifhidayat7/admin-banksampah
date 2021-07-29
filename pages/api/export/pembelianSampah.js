6f8fdf165fa5f647e4cd03dc009ca09ea030fceb
Arif Hidayat <moharifhidayat7@gmail.com>
2021-07-29 09:49:17 +0700

    asd

--- pages/api/export/pembelianSampah.js
+++ pages/api/export/pembelianSampah.js
@@ -1,172 +1,166 @@
 import createHandler from "../../../src/middleware/index";
-import SampahPurchase from "../../../src/models/SampahPurchase";
-import SampahType from "../../../src/models/SampahType";
 import ExcelJS from "exceljs";
 
 const handler = createHandler();
 
 handler.get(async (req, res) => {
-    const queryTipe = req.query.tipe || "tabung"
-    const workbook = new ExcelJS.Workbook();
-    await workbook.xlsx.readFile(
-        "./src/export/export-rekap-sampah-harian.xlsx"
-    );
-    const sheet = workbook.worksheets[0];
-    sheet.getRow(1).getCell(1).value = "BULAN MARET 2021";
-    if(queryTipe == "tabung"){
-        sheet.getRow(3).getCell(19).value = "TABUNGAN";
-    } else if (queryTipe == 'cash'){
-        sheet.getRow(3).getCell(19).value = "CASH";
-    }
-    const month = req.query.month || "";
-    const result = await SampahPurchase.find({
-        transactionDate: {
-            $regex: month,
-        },
+  const queryTipe = req.query.tipe || "tabung";
+  const workbook = new ExcelJS.Workbook();
+  await workbook.xlsx.readFile("./src/export/export-rekap-sampah-harian.xlsx");
+  const sheet = workbook.worksheets[0];
+  sheet.getRow(1).getCell(1).value = "BULAN MARET 2021";
+  if (queryTipe == "tabung") {
+    sheet.getRow(3).getCell(19).value = "TABUNGAN";
+  } else if (queryTipe == "cash") {
+    sheet.getRow(3).getCell(19).value = "CASH";
+  }
+  const month = req.query.month || "";
+  const result = await SampahPurchase.find({
+    transactionDate: {
+      $regex: month,
+    },
+  });
+  const cashResult = result.filter((c) => c.transactionType == "CASH");
+  const tabungResult = result.filter((t) => t.transactionType == "TABUNG");
+
+  const types = await SampahType.aggregate([
+    {
+      $group: {
+        _id: "$category",
+        items: { $push: "$$ROOT" },
+      },
+    },
+  ]);
+
+  const reversed = types.reverse();
+
+  const cash = [];
+  const cash2 = [];
+  const tabung = [];
+  const tabung2 = [];
+
+  for (let i = 0; i < cashResult.length; i++) {
+    cash.push(...cashResult[i].items);
+    cash2.push(cashResult[i].items);
+  }
+  for (let i = 0; i < tabungResult.length; i++) {
+    tabung.push(...tabungResult[i].items);
+    tabung2.push(tabungResult[i].items);
+  }
+
+  const group = reversed.map((g) => {
+    const items = g.items.map((type) => {
+      const rekapCash = cash.filter((a) => a._sampahType._id == type._id);
+
+      const cashSum = rekapCash.reduce((tot, item) => {
+        const subTotal = item._sampahType.price * item.qty;
+        return tot + subTotal;
+      }, 0);
+
+      const cashQty = rekapCash.reduce((tot, item) => {
+        return tot + item.qty;
+      }, 0);
+
+      const rekapTabung = tabung.filter((a) => a._sampahType._id == type._id);
+
+      const tabungSum = rekapTabung.reduce((tot, item) => {
+        const subTotal = item._sampahType.price * item.qty;
+        return tot + subTotal;
+      }, 0);
+
+      const tabungQty = rekapTabung.reduce((tot, item) => {
+        return tot + item.qty;
+      }, 0);
+      return {
+        _id: type._id,
+        type: type.name,
+        price: type.price,
+        denom: type.denom,
+        cashQty: cashQty,
+        cashPrice: cashSum,
+        tabungQty: tabungQty,
+        tabungPrice: tabungSum,
+      };
     });
-    const cashResult = result.filter((c) => c.transactionType == "CASH");
-    const tabungResult = result.filter((t) => t.transactionType == "TABUNG");
-
-    const types = await SampahType.aggregate([
-        {
-            $group: {
-                _id: "$category",
-                items: { $push: "$$ROOT" },
-            },
-        },
-    ]);
-
-    const reversed = types.reverse();
-
-    const cash = [];
-    const cash2 = [];
-    const tabung = [];
-    const tabung2 = [];
-
-    for (let i = 0; i < cashResult.length; i++) {
-        cash.push(...cashResult[i].items);
-        cash2.push(cashResult[i].items);
-    }
-    for (let i = 0; i < tabungResult.length; i++) {
-        tabung.push(...tabungResult[i].items);
-        tabung2.push(tabungResult[i].items);
-    }
-
-    const group = reversed.map((g) => {
-        const items = g.items.map((type) => {
-            const rekapCash = cash.filter((a) => a._sampahType._id == type._id);
-
-            const cashSum = rekapCash.reduce((tot, item) => {
-                const subTotal = item._sampahType.price * item.qty;
-                return tot + subTotal;
-            }, 0);
-
-            const cashQty = rekapCash.reduce((tot, item) => {
-                return tot + item.qty;
-            }, 0);
-
-            const rekapTabung = tabung.filter(
-                (a) => a._sampahType._id == type._id
-            );
-
-            const tabungSum = rekapTabung.reduce((tot, item) => {
-                const subTotal = item._sampahType.price * item.qty;
-                return tot + subTotal;
-            }, 0);
-
-            const tabungQty = rekapTabung.reduce((tot, item) => {
-                return tot + item.qty;
-            }, 0);
-            return {
-                _id: type._id,
-                type: type.name,
-                price: type.price,
-                denom: type.denom,
-                cashQty: cashQty,
-                cashPrice: cashSum,
-                tabungQty: tabungQty,
-                tabungPrice: tabungSum,
-            };
-        });
-        return {
-            category: g._id,
-            items: items.filter((item) => {
-                if (item.cashQty > 0 || item.tabungQty > 0) {
-                    return item;
-                }
-            }),
-        };
-    });
-
-    const finalData = group.filter((grouped) => grouped.items.length > 0);
-
-    let rowStart = 7;
-
-    for (let i = 0; i < finalData.length; i++) {
-        const data = finalData[i];
-        const categoryRow = sheet.getRow(rowStart);
-        categoryRow.getCell(1).value = i + 1;
-        categoryRow.getCell(2).value = data.category.toUpperCase();
-
-        for (let j = 0; j < data.items.length; j++) {
-            const item = data.items[j];
-
-            const itemRowStart = sheet.getRow(rowStart + 1);
-            itemRowStart.getCell(2).value = item.type.toUpperCase();
-            itemRowStart.getCell(3).value = item.price;
-            
-            let qtyTotal = 0;
-            let totalPrice = 0;
-            let getResult;
-            if(queryTipe == "tabung"){
-                getResult = tabungResult
-            } else if (queryTipe == 'cash'){
-                getResult = cashResult
-            }
-            for (let k = 0; k < getResult.length; k++) {
-                const trx = getResult[k];
-                for (let l = 0; l < trx.items.length; l++) {
-                    const trxItems = trx.items[l];
-                    if (trxItems._sampahType._id == item._id) {
-                        itemRowStart.getCell(4+k).value = trxItems.qty
-                        qtyTotal += trxItems.qty
-                        totalPrice += trxItems.qty * trxItems._sampahType.price
-                    }
-                }
-                
-            }
-            itemRowStart.getCell(19).value = {
-                formula: `SUM(D${rowStart + 1}:R${rowStart + 1})`,
-                result: qtyTotal,
-            };
-            itemRowStart.getCell(20).value = {
-                formula: `S${rowStart + 1}*C${rowStart + 1}`,
-                result: totalPrice,
-            };
-            
-            rowStart += 1;
+    return {
+      category: g._id,
+      items: items.filter((item) => {
+        if (item.cashQty > 0 || item.tabungQty > 0) {
+          return item;
         }
-        rowStart += 1;
-    }
-    const filedate = Date.now();
-    // await workbook.xlsx.writeFile(
-    //     `./src/export/generated/export-rekap-sampah-harian-${new Date()
-    //         .toJSON()
-    //         .slice(0, 7)}-${filedate}.xlsx`
-    // );
-    res.setHeader(
-        "Content-Type",
-        "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
-    );
-    res.setHeader(
-        "Content-Disposition",
-        "attachment; filename=" + `export-rekap-sampah-masuk-${queryTipe}-${filedate}.xlsx`
-    );
+      }),
+    };
+  });
 
-    return workbook.xlsx.write(res).then(function () {
-        res.status(200).end();
-    });
-    res.status(200).json(finalData);
+  const finalData = group.filter((grouped) => grouped.items.length > 0);
+
+  let rowStart = 7;
+
+  for (let i = 0; i < finalData.length; i++) {
+    const data = finalData[i];
+    const categoryRow = sheet.getRow(rowStart);
+    categoryRow.getCell(1).value = i + 1;
+    categoryRow.getCell(2).value = data.category.toUpperCase();
+
+    for (let j = 0; j < data.items.length; j++) {
+      const item = data.items[j];
+
+      const itemRowStart = sheet.getRow(rowStart + 1);
+      itemRowStart.getCell(2).value = item.type.toUpperCase();
+      itemRowStart.getCell(3).value = item.price;
+
+      let qtyTotal = 0;
+      let totalPrice = 0;
+      let getResult;
+      if (queryTipe == "tabung") {
+        getResult = tabungResult;
+      } else if (queryTipe == "cash") {
+        getResult = cashResult;
+      }
+      for (let k = 0; k < getResult.length; k++) {
+        const trx = getResult[k];
+        for (let l = 0; l < trx.items.length; l++) {
+          const trxItems = trx.items[l];
+          if (trxItems._sampahType._id == item._id) {
+            itemRowStart.getCell(4 + k).value = trxItems.qty;
+            qtyTotal += trxItems.qty;
+            totalPrice += trxItems.qty * trxItems._sampahType.price;
+          }
+        }
+      }
+      itemRowStart.getCell(19).value = {
+        formula: `SUM(D${rowStart + 1}:R${rowStart + 1})`,
+        result: qtyTotal,
+      };
+      itemRowStart.getCell(20).value = {
+        formula: `S${rowStart + 1}*C${rowStart + 1}`,
+        result: totalPrice,
+      };
+
+      rowStart += 1;
+    }
+    rowStart += 1;
+  }
+  const filedate = Date.now();
+  // await workbook.xlsx.writeFile(
+  //     `./src/export/generated/export-rekap-sampah-harian-${new Date()
+  //         .toJSON()
+  //         .slice(0, 7)}-${filedate}.xlsx`
+  // );
+  res.setHeader(
+    "Content-Type",
+    "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+  );
+  res.setHeader(
+    "Content-Disposition",
+    "attachment; filename=" +
+      `export-rekap-sampah-masuk-${queryTipe}-${filedate}.xlsx`
+  );
+
+  return workbook.xlsx.write(res).then(function () {
+    res.status(200).end();
+  });
+  res.status(200).json(finalData);
 });
 
 export default handler;
--- pages/api/stok.js
+++ pages/api/stok.js
@@ -1,66 +1,65 @@
 import createHandler from "../../src/middleware/index";
-import SampahPurchase from "../../src/models/SampahPurchase";
 import SampahType from "../../src/models/SampahType";
 
 const handler = createHandler();
 
 handler.get(async (req, res) => {
-    const month = req.query.month || "";
-    const result = await SampahPurchase.find({
-        transactionDate: {
-            $regex: month,
-        },
-    });
-    const cashResult = result.filter((c) => c.transactionType == "CASH");
-    const tabungResult = result.filter((t) => t.transactionType == "TABUNG");
+  const month = req.query.month || "";
+  const result = await SampahPurchase.find({
+    transactionDate: {
+      $regex: month,
+    },
+  });
+  const cashResult = result.filter((c) => c.transactionType == "CASH");
+  const tabungResult = result.filter((t) => t.transactionType == "TABUNG");
 
-    const types = await SampahType.find();
-    const cash = [];
-    const tabung = [];
+  const types = await SampahType.find();
+  const cash = [];
+  const tabung = [];
 
-    for (let i = 0; i < cashResult.length; i++) {
-        cash.push(...cashResult[i].items);
-    }
-    for (let i = 0; i < tabungResult.length; i++) {
-        tabung.push(...tabungResult[i].items);
-    }
+  for (let i = 0; i < cashResult.length; i++) {
+    cash.push(...cashResult[i].items);
+  }
+  for (let i = 0; i < tabungResult.length; i++) {
+    tabung.push(...tabungResult[i].items);
+  }
 
-    const group = types.map((type) => {
-        const rekapCash = cash.filter((a) => a._sampahType._id == type._id);
+  const group = types.map((type) => {
+    const rekapCash = cash.filter((a) => a._sampahType._id == type._id);
 
-        const cashSum = rekapCash.reduce((tot, item) => {
-            const subTotal = item._sampahType.price * item.qty;
-            return tot + subTotal;
-        }, 0);
+    const cashSum = rekapCash.reduce((tot, item) => {
+      const subTotal = item._sampahType.price * item.qty;
+      return tot + subTotal;
+    }, 0);
 
-        const cashQty = rekapCash.reduce((tot, item) => {
-            return tot + item.qty;
-        }, 0);
+    const cashQty = rekapCash.reduce((tot, item) => {
+      return tot + item.qty;
+    }, 0);
 
-        const rekapTabung = tabung.filter((a) => a._sampahType._id == type._id);
+    const rekapTabung = tabung.filter((a) => a._sampahType._id == type._id);
 
-        const tabungSum = rekapTabung.reduce((tot, item) => {
-            const subTotal = item._sampahType.price * item.qty;
-            return tot + subTotal;
-        }, 0);
+    const tabungSum = rekapTabung.reduce((tot, item) => {
+      const subTotal = item._sampahType.price * item.qty;
+      return tot + subTotal;
+    }, 0);
 
-        const tabungQty = rekapTabung.reduce((tot, item) => {
-            return tot + item.qty;
-        }, 0);
+    const tabungQty = rekapTabung.reduce((tot, item) => {
+      return tot + item.qty;
+    }, 0);
 
-        return {
-            _id: type._id,
-            category: type.category,
-            type: type.name,
-            denom: type.denom,
-            cashQty: cashQty,
-            cashPrice: cashSum,
-            tabungQty: tabungQty,
-            tabungPrice: tabungSum,
-        };
-    });
+    return {
+      _id: type._id,
+      category: type.category,
+      type: type.name,
+      denom: type.denom,
+      cashQty: cashQty,
+      cashPrice: cashSum,
+      tabungQty: tabungQty,
+      tabungPrice: tabungSum,
+    };
+  });
 
-    res.status(200).json(group.filter((q) => q.cashQty > 0 || q.tabungQty > 0));
+  res.status(200).json(group.filter((q) => q.cashQty > 0 || q.tabungQty > 0));
 });
 
 export default handler;
